5e05509b250497d7aaf73bf00b2130487f06b713
DevArrowsmith <devarrowsmith@gmail.com>
2021-08-05 14:04:29 +0100

    Assign magic numbers in babiesSorter to named variables.

--- babiesSorter.js
+++ babiesSorter.js
@@ -1,11 +1,14 @@
+const CHARACTER_CODE_OF_a = 97
+const DIFFERENCE_BETWEEN_UPPER_AND_LOWER_CASE_CHARACTER_CODES_PLUS_HALF = 31.5
+
 const babiesSorter = (a, b) => {
 
     const aCharCode = a.charCodeAt(0);
     const bCharCode = b.charCodeAt(0);
 
     const reformatCharCode = (charCode) =>
-        charCode >= 96
-        ? charCode - 31.5
+        charCode >= CHARACTER_CODE_OF_a
+        ? charCode - DIFFERENCE_BETWEEN_UPPER_AND_LOWER_CASE_CHARACTER_CODES_PLUS_HALF
         : charCode;
 
     const aValue = reformatCharCode(aCharCode);
